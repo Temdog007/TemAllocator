ef06f4fc4d3bd4a2ca7ce07fff69a2f0840a17c6
Temdog007 <temdog007@yahoo.com>
2022-07-12 06:01:30 -0700

    Initial comimt

---
+++ allocator.hpp
@@ -0,0 +1,574 @@
+/******************************************************************************
+	Copyright (C) 2022 by Temitope Alaga <temdog007@yaoo.com>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#pragma once
+
+#include <mutex>
+
+namespace TemAllocator
+{
+	/**
+	 * @brief Placement policy for free list allocator
+	 */
+	enum class PlacementPolicy
+	{
+		First, ///< Find first block that is big enough the handle the allocation request. The faster policy.
+		Best
+		///< Find smallest block that is big enough the handle the allocation request. Reduces chance of fragmentation
+		///< in heap. The slower policy
+	};
+	/**
+	 * @brief Linked list used by free list allocator
+	 */
+	struct FreeListNode
+	{
+		size_t blockSize;
+		FreeListNode *next;
+
+		static void insert(FreeListNode *&head, FreeListNode *previous, FreeListNode *newNode)
+		{
+			if (previous == nullptr)
+			{
+				newNode->next = head;
+				head = newNode;
+			}
+			else
+			{
+				newNode->next = previous->next;
+				previous->next = newNode;
+			}
+		}
+		static void remove(FreeListNode *&head, FreeListNode *previous, FreeListNode *deleteNode)
+		{
+			if (previous == nullptr)
+			{
+				head = deleteNode->next;
+			}
+			else
+			{
+				previous->next = deleteNode->next;
+			}
+		}
+	};
+
+	template <class T>
+	class Allocator;
+
+	/**
+	 * @brief Data passed to all free list allocators
+	 */
+	class AllocatorData
+	{
+	private:
+		std::mutex mutex;
+		FreeListNode *list;
+		void *data;
+		size_t used;
+		size_t len;
+		size_t allocationNum;
+		PlacementPolicy policy;
+
+		template <class T>
+		friend class Allocator;
+
+		void reset()
+		{
+			FreeListNode *first = reinterpret_cast<FreeListNode *>(data);
+			first->blockSize = len;
+			first->next = nullptr;
+			used = 0;
+			list = nullptr;
+			FreeListNode::insert(list, nullptr, first);
+		}
+
+		void close()
+		{
+			if (data != nullptr)
+			{
+				free(data);
+				data = nullptr;
+			}
+		}
+
+		/**
+		 * @brief Combine memory blocks if possible
+		 *
+		 * @param previousNode Node that came before freeNode
+		 * @param freeNode Node that was just re-inserted into memory blcok
+		 */
+		void coalescence(FreeListNode *previousNode, FreeListNode *freeNode)
+		{
+			if (freeNode->next != nullptr &&
+				reinterpret_cast<size_t>(freeNode) + freeNode->blockSize == reinterpret_cast<size_t>(freeNode->next))
+			{
+				freeNode->blockSize += freeNode->next->blockSize;
+				FreeListNode::remove(list, freeNode, freeNode->next);
+			}
+			if (previousNode != nullptr &&
+				reinterpret_cast<size_t>(previousNode) + previousNode->blockSize == reinterpret_cast<size_t>(freeNode))
+			{
+				previousNode->blockSize += freeNode->blockSize;
+				FreeListNode::remove(list, previousNode, freeNode);
+			}
+		}
+
+		/**
+		 * @brief Find a valid memory block
+		 *
+		 * @param size Requested memory block size
+		 * @param previousNode [out] the node before the foundNode
+		 * @param foundNode [out] the node that contains the request memory block
+		 */
+		void find(const size_t size, FreeListNode *&previousNode, FreeListNode *&foundNode)
+		{
+			switch (policy)
+			{
+			case PlacementPolicy::First:
+				findFirst(size, previousNode, foundNode);
+				break;
+			case PlacementPolicy::Best:
+				findBest(size, previousNode, foundNode);
+				break;
+			default:
+				break;
+			}
+		}
+
+		/**
+		 * @brief See #TemAllocator::PlacementPolicy::First
+		 *
+		 * @param size Requested memory block size
+		 * @param previousNode [out] the node before the foundNode
+		 * @param foundNode [out] the node that contains the request memory block
+		 */
+		void findFirst(const size_t size, FreeListNode *&previousNode, FreeListNode *&foundNode)
+		{
+			FreeListNode *it = list;
+			FreeListNode *prev = nullptr;
+			while (it != nullptr)
+			{
+				if (it->blockSize >= size)
+				{
+					break;
+				}
+				prev = it;
+				it = it->next;
+			}
+			previousNode = prev;
+			foundNode = it;
+		}
+
+		/**
+		 * @brief See #TemAllocator::PlacementPolicy::Best
+		 *
+		 * @param size Requested memory block size
+		 * @param previousNode [out] the node before the foundNode
+		 * @param foundNode [out] the node that contains the request memory block
+		 */
+		void findBest(const size_t size, FreeListNode *&previousNode, FreeListNode *&foundNode)
+		{
+			size_t smallestDiff = SIZE_MAX;
+			FreeListNode *bestBlock = nullptr;
+			FreeListNode *bestPrevBlock = nullptr;
+			FreeListNode *it = list;
+			FreeListNode *prev = nullptr;
+			while (it != nullptr)
+			{
+				const size_t currentDiff = it->blockSize - size;
+				if (it->blockSize >= size && currentDiff < smallestDiff)
+				{
+					bestBlock = it;
+					bestPrevBlock = prev;
+					smallestDiff = currentDiff;
+				}
+				prev = it;
+				it = it->next;
+			}
+			previousNode = bestPrevBlock;
+			foundNode = bestBlock;
+		}
+
+	public:
+		AllocatorData() noexcept
+			: mutex(), list(nullptr), data(nullptr), used(0), len(0), allocationNum(0), policy(PlacementPolicy::Best)
+		{
+		}
+		AllocatorData(const AllocatorData &) = delete;
+		AllocatorData(AllocatorData &&) = delete;
+
+		~AllocatorData()
+		{
+			close();
+		}
+
+		/**
+		 * @brief Get total availble memory
+		 *
+		 * @return total available memory in bytes
+		 */
+		size_t getTotal() const
+		{
+			return len;
+		}
+
+		/**
+		 * @brief Get amount of memory currently in use
+		 *
+		 * @return memory in use in bytes
+		 */
+		size_t getUsed() const
+		{
+			return used;
+		}
+
+		/**
+		 * @brief Get number of allocation calls
+		 *
+		 * @return Number of allocation calls
+		 */
+		size_t getNum() const
+		{
+			return allocationNum;
+		}
+
+		/**
+		 * Reset and re-allocate data. Does NOT re-assign pointers that were using the old memory block. Only use at startup
+		 *
+		 * @param len Amount of total memory to use
+		 * @param policy
+		 */
+		void init(const size_t len, PlacementPolicy policy = PlacementPolicy::Best)
+		{
+			close();
+
+			list = nullptr;
+			used = 0;
+			this->len = len;
+			data = malloc(len);
+			this->policy = policy;
+			reset();
+		}
+	};
+
+	/**
+	 * Data used by default constructor of Allocators
+	 *
+	 * @ref TemAllocator::Allocator
+	 */
+	extern AllocatorData globalAllocatorData;
+
+	/**
+	 * @brief Free list allocator
+	 *
+	 * @tparam T type to allocate
+	 */
+	template <class T>
+	class Allocator
+	{
+	public:
+		typedef T value_type;
+
+		template <class U>
+		friend class Allocator;
+
+	private:
+		AllocatorData &ad;
+
+	public:
+		Allocator() noexcept : ad(globalAllocatorData)
+		{
+		}
+		Allocator(AllocatorData &ad) noexcept : ad(ad)
+		{
+		}
+		~Allocator()
+		{
+		}
+
+		template <class U>
+		Allocator(const Allocator<U> &u) noexcept : ad(u.ad)
+		{
+		}
+		template <class U>
+		bool operator==(const Allocator<U> &) const noexcept
+		{
+			return true;
+		}
+		template <class U>
+		bool operator!=(const Allocator<U> &) const noexcept
+		{
+			return false;
+		}
+
+		/**
+		 * @brief Allocate a number of type T
+		 *
+		 * @param n Number of T's to allocate
+		 *
+		 * @return pointer to allocated data
+		 */
+		T *allocate(const size_t n = 1);
+
+		/**
+		 * @brief Re-allocate a number of type T.
+		 *
+		 * Try to extend the current block. If not possible, allocate new block, copy old block data to new block data, and
+		 * then free old block.
+		 *
+		 * @param ptr Pointer to the old data
+		 * @param n Number of T's to allocate
+		 *
+		 * @return pointer to allocated data
+		 */
+		T *reallocate(T *ptr, const size_t n);
+
+		/**
+		 * @brief De-allocate the pointer
+		 *
+		 * @param p The pointer to free
+		 * @param count unused
+		 */
+		void deallocate(T *const p, const size_t count = 1);
+
+		/**
+		 * @brief Call constructor on pointer with arguments
+		 *
+		 * @tparam Args the arguments to pass to the constructor
+		 * @param t The pointer
+		 * @param args the argumetns
+		 */
+		template <typename... Args>
+		static void construct(T *t, Args &&...args)
+		{
+			new (t) T(std::forward<Args>(args)...);
+		}
+
+		/**
+		 * @brief Call destructor on pointer
+		 *
+		 * @param p the pointer
+		 */
+		static void destroy(T *p)
+		{
+			p->~T();
+		}
+
+		/**
+		 * @brief Get size of block from pointer
+		 *
+		 * @param p the poitner
+		 *
+		 * @return The size of the block
+		 */
+		size_t getBlockSize(const T *const p) const;
+	};
+
+#if __ANDROID__
+#define ALLOCATOR_ALIGNMENT 16
+#else
+#define ALLOCATOR_ALIGNMENT (2 * sizeof(void *))
+#endif
+
+	template <class T>
+	T *Allocator<T>::allocate(const size_t requestedCount)
+	{
+		// STL containers will call allocate with size 0. So, nullptr is valid
+		if (requestedCount == 0)
+		{
+			return nullptr;
+		}
+
+		const size_t requestedSize = sizeof(T) * requestedCount;
+
+		std::lock_guard<std::mutex> g(ad.mutex);
+
+		// Align memory just to be safe
+		size_t size = std::max<size_t>(requestedSize, ALLOCATOR_ALIGNMENT);
+		size += ALLOCATOR_ALIGNMENT - (size % ALLOCATOR_ALIGNMENT);
+		const size_t allocateSize = size + sizeof(FreeListNode);
+
+		FreeListNode *affectedNode = nullptr;
+		FreeListNode *previousNode = nullptr;
+		ad.find(allocateSize, previousNode, affectedNode);
+
+		// If null, then there is no block that can handle the requestedSize
+		if (affectedNode == nullptr)
+		{
+			throw std::bad_alloc();
+		}
+
+		const size_t rest = affectedNode->blockSize - allocateSize;
+
+		// If block has extra size, split the block into 2 and insert the remaining chunk back
+		// into the linked list
+		if (rest > 0)
+		{
+			FreeListNode *newFreeNode =
+				reinterpret_cast<FreeListNode *>(reinterpret_cast<size_t>(affectedNode) + allocateSize);
+			newFreeNode->blockSize = rest;
+			newFreeNode->next = nullptr;
+			FreeListNode::insert(ad.list, affectedNode, newFreeNode);
+		}
+
+		// Remove the allocated data from the linked list.
+		FreeListNode::remove(ad.list, previousNode, affectedNode);
+		affectedNode->blockSize = allocateSize;
+		affectedNode->next = nullptr;
+
+		ad.used += allocateSize;
+
+		const size_t dataAddress = reinterpret_cast<size_t>(affectedNode) + sizeof(FreeListNode);
+		T *ptr = reinterpret_cast<T *>(dataAddress);
+		++ad.allocationNum;
+		return ptr;
+	}
+	template <class T>
+	T *Allocator<T>::reallocate(T *oldPtr, const size_t count)
+	{
+		std::lock_guard<std::mutex> g(ad.mutex);
+
+		if (oldPtr == nullptr)
+		{
+			return allocate(count);
+		}
+
+		// Align memory just to be safe
+		size_t size = sizeof(T) * count;
+		size = std::max<size_t>(size, ALLOCATOR_ALIGNMENT);
+		size += ALLOCATOR_ALIGNMENT - (size % ALLOCATOR_ALIGNMENT);
+
+		// Get the current memory block
+		const size_t currentAddress = (size_t)oldPtr;
+		const size_t nodeAddress = currentAddress - sizeof(FreeListNode);
+
+		FreeListNode *node = reinterpret_cast<FreeListNode *>(nodeAddress);
+
+		const size_t oldSize = node->blockSize - sizeof(FreeListNode);
+
+		// Don't reduce the size of the current block. Just return.
+		if (size <= oldSize)
+		{
+			return oldPtr;
+		}
+
+		{
+			// Find the block that would be right after the current block. That is the only block that can be used to
+			// extending the current block. Also, find the block before it in the linked list
+			const size_t target = nodeAddress + node->blockSize;
+			FreeListNode *it = ad.list;
+			FreeListNode *prev = NULL;
+			while (it != NULL)
+			{
+				if (reinterpret_cast<size_t>(it) != target)
+				{
+					prev = it;
+					it = it->next;
+					continue;
+				}
+
+				// The size of the current block and the block after it if they were combined
+				const size_t combinedSize = node->blockSize + it->blockSize;
+
+				// The size of the re-allocated block
+				const size_t newBlockSize = size + sizeof(FreeListNode);
+
+				// If the size of the two blocks is exactly the requested size, then just remove the block
+				if (combinedSize == newBlockSize)
+				{
+					ad.used -= node->blockSize;
+					ad.used += newBlockSize;
+					node->blockSize = newBlockSize;
+					FreeListNode::remove(ad.list, prev, it);
+					return oldPtr;
+				}
+
+				// If the combined size is greater than the requested size, the block will need to be split. Then, the
+				// remaining chunk can be inserted back into the list.
+				else if (newBlockSize < combinedSize)
+				{
+					ad.used -= node->blockSize;
+					ad.used += newBlockSize;
+					node->blockSize = newBlockSize;
+					FreeListNode *newNode = reinterpret_cast<FreeListNode *>(reinterpret_cast<size_t>(node) + newBlockSize);
+					newNode->blockSize = combinedSize - newBlockSize;
+					newNode->next = nullptr;
+					FreeListNode::remove(ad.list, prev, it);
+					FreeListNode::insert(ad.list, prev, newNode);
+					return oldPtr;
+				}
+
+				// Not possible to re-allocate. Exit loop
+				break;
+			}
+		}
+
+		// At this point, it is determined that re-allocating is not possible.
+		// So, allocate new block, copy old block to new block, and free old block
+		T *newPtr = allocate(count);
+		memcpy(newPtr, oldPtr, oldSize);
+		deallocate(oldPtr);
+		return newPtr;
+	}
+	template <class T>
+	void Allocator<T>::deallocate(T *const ptr, const size_t)
+	{
+		if (ptr == nullptr)
+		{
+			return;
+		}
+
+		std::lock_guard<std::mutex> g(ad.mutex);
+
+		const size_t currentAddress = reinterpret_cast<size_t>(ptr);
+		const size_t headerAddress = currentAddress - sizeof(FreeListNode);
+
+		FreeListNode *freeNode = reinterpret_cast<FreeListNode *>(headerAddress);
+		freeNode->next = nullptr;
+
+		FreeListNode *it = ad.list;
+		FreeListNode *prev = nullptr;
+
+		// Insert the block back into the list at the right spot
+		while (it != nullptr)
+		{
+			if (freeNode < it)
+			{
+				FreeListNode::insert(ad.list, prev, freeNode);
+				break;
+			}
+			prev = it;
+			it = it->next;
+		}
+
+		ad.used -= freeNode->blockSize;
+
+		// Combine adjacent blocks into one
+		ad.coalescence(prev, freeNode);
+		--ad.allocationNum;
+	}
+	template <class T>
+	size_t Allocator<T>::getBlockSize(const T *const ptr) const
+	{
+		if (ptr == nullptr)
+		{
+			return 0;
+		}
+		std::lock_guard<std::mutex> g(ad.mutex);
+
+		const size_t currentAddress = reinterpret_cast<size_t>(ptr);
+		const size_t headerAddress = currentAddress - sizeof(FreeListNode);
+		const FreeListNode *freeNode = reinterpret_cast<const FreeListNode *>(headerAddress);
+		return freeNode->blockSize;
+	}
+} // namespace TemAllocator
---
+++ allocator_defs.hpp
@@ -0,0 +1,147 @@
+/******************************************************************************
+	Copyright (C) 2022 by Temitope Alaga <temdog007@yaoo.com>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#pragma once
+
+#include "allocator.hpp"
+
+#include <string>
+#include <codecvt>
+#include <locale>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <memory>
+#include <vector>
+#include <set>
+#include <list>
+#include <cmath>
+#include <deque>
+#include <queue>
+
+namespace TemAllocator
+{
+	using String = std::basic_string<char, std::char_traits<char>, Allocator<char>>;
+	using String32 = std::basic_string<char32_t, std::char_traits<char32_t>, Allocator<char32_t>>;
+	using StringStream = std::basic_stringstream<char, std::char_traits<char>, Allocator<char>>;
+
+	template <typename T>
+	using List = std::vector<T, Allocator<T>>;
+
+	template <typename T>
+	using Deque = std::deque<T, Allocator<T>>;
+
+	template <typename K>
+	using Set = std::unordered_set<K, std::hash<K>, std::equal_to<K>, Allocator<K>>;
+
+	template <typename K>
+	using OrderedSet = std::set<K, std::less<K>, Allocator<K>>;
+
+	template <typename K, typename V>
+	using Map = std::unordered_map<K, V, std::hash<K>, std::equal_to<K>, Allocator<std::pair<const K, V>>>;
+
+	template <typename K, typename V>
+	using OrderedMap = std::map<K, V, std::less<K>, Allocator<std::pair<const K, V>>>;
+
+	template <typename T>
+	using LinkedList = std::list<T, Allocator<T>>;
+
+	template <typename T>
+	using Queue = std::queue<T, LinkedList<T>>;
+
+	template <typename T>
+	using Stack = std::stack<T, Allocator<T>>;
+
+	using UTF8Converter = std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t, Allocator<char32_t>, Allocator<char>>;
+
+	template <typename T, typename... Args>
+	static inline T *allocateAndConstruct(Args &&...args)
+	{
+		Allocator<T> a;
+		T *t = a.allocate(1);
+		a.construct(t, std::forward<Args>(args)...);
+		return t;
+	}
+
+	template <typename T>
+	static inline void destroyAndDeallocate(T *const t)
+	{
+		Allocator<T> a;
+		a.destroy(t);
+		a.deallocate(t);
+	}
+
+	template <typename T>
+	struct Deleter
+	{
+		constexpr Deleter() noexcept = default;
+#if __unix__
+		template <typename U, typename = std::_Require<std::is_convertible<U *, T *>>>
+		constexpr Deleter(Deleter<U>) noexcept
+		{
+		}
+#else
+		template <typename U>
+		constexpr Deleter(Deleter<U>) noexcept
+		{
+		}
+#endif
+		void operator()(T *t) const
+		{
+			destroyAndDeallocate(t);
+		}
+	};
+
+	template <typename T>
+	using unique_ptr = std::unique_ptr<T, Deleter<T>>;
+
+	template <typename T>
+	using shared_ptr = std::shared_ptr<T>;
+
+	template <typename T, typename... Args>
+	static inline unique_ptr<T> make_unique(Args &&...args)
+	{
+		return unique_ptr<T>(allocateAndConstruct<T>(std::forward<Args>(args)...), Deleter<T>());
+	}
+
+	template <typename T, typename... Args>
+	static inline shared_ptr<T> make_shared(Args &&...args)
+	{
+		return shared_ptr<T>(allocateAndConstruct<T>(std::forward<Args>(args)...), Deleter<T>());
+	}
+
+	template <typename T>
+	static inline shared_ptr<T> make_shared(T *t)
+	{
+		return shared_ptr<T>(t, Deleter<T>());
+	}
+} // namespace TemAllocator
+
+namespace std
+{
+	template <>
+	struct hash<TemAllocator::String>
+	{
+		std::size_t operator()(const TemAllocator::String &s) const noexcept
+		{
+			std::size_t value = 0;
+			for (size_t i = 0; i < s.size(); ++i)
+			{
+				value += s[i] * std::pow(size_t(31), i);
+			}
+			return value;
+		}
+	};
+} // namespace std
